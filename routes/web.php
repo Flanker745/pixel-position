ae718fcfaaec1fc2d220c9559ed02f12e4564e89
Flanker745 <pradeepnain745@gmail.com>
2024-07-02 18:01:22 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,22 @@ use App\Http\Controllers\tagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, "index"]);
-Route::get("/search" , searchController::class);
-Route::get("/tags/{tag:name}" , tagController::class);
-Route::get("/jobs" , [JobController::class, "create"]);
-Route::post("/jobs" , [JobController::class, "store"]);
+Route::get("/search", searchController::class);
+Route::get("/tags/{tag:name}", tagController::class);
+
 
 
 Route::middleware("guest")->group(function () {
+
+
     Route::get("/register", [RegisterController::class, "create"]);
     Route::post("/register", [RegisterController::class, "store"]);
 
-    Route::get("/login", [LoginController::class, "create"]);
+    Route::get("/login", [LoginController::class, "create"])->name("login");
     Route::post("/login", [LoginController::class, "store"]);
 });
-
-Route::delete("/logout", [LoginController::class, "destroy"])->middleware("auth");
+Route::middleware("auth")->group(function () {
+    Route::get("/jobs", [JobController::class, "create"]);
+    Route::post("/jobs", [JobController::class, "store"]);
+    Route::delete("/logout", [LoginController::class, "destroy"]);
+});
